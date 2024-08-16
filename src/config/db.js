7a6fd83643283088473d58676277ca92ec57ca1c
Suresh-vivek <svivek.kumar012@gmail.com>
2024-08-16 14:43:32 +0530

    Added Database Configuration

---
+++ src/config/db.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+// load env vars
+dotenv.config();
+console.log("MONGO_URI:", process.env.MONGO_URI);
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
